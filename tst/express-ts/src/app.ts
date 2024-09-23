ea56432928c5232b5250dd19639b1f02d631c84e
JrKh16 <jirawatkhunngean@gmail.com>
2024-09-23 21:38:38 +0700

    create svelte connect api

--- tst/express-ts/src/app.ts
+++ tst/express-ts/src/app.ts
@@ -1,34 +1,42 @@
-import "dotenv/config" 
+import "dotenv/config"
+import cors from "cors" 
 import express, { Express, Request, Response,NextFunction } from 'express'
-import cors from 'cors'
-import { myapiRoute } from "./lib/myapi"
-import { fruitRoute } from "./lib/fruit"
-
+import {myapiRoute} from './lib/myapi'
+import {fruitRoute} from "./lib/fruit"
+import Router from "./routes"
+import swaggerUi from "swagger-ui-express"
 const app: Express = express()
-const port = Number(process.env.PORT) || 3000
-
-app.use('/api/myapi',myapiRoute)
-app.use('/api/fruits',fruitRoute)
+const port = Number(process.env.PORT) || 80
+/*
+let apikey="123456789"
+app.use((req:Request,res:Response,next:NextFunction)=>{
+  console.log("Middleware")
+  if(req.headers.apikey!==apikey ){
+    return res.status(401).json({error:"Unauthorized"})
+  }
+  next()
+})
+*/
 app.use(cors())
 app.use(express.json())
 app.use(express.raw())
 app.use(express.urlencoded({extended:true}))
-
-
-// let apikey="123456789"
-// app.use((req:Request,res:Response,next:NextFunction)=>{
-//   console.log("Middleware")
-//   if(req.headers.apikey!==apikey ){
-//     return res.status(401).json({error:"Unauthorized"})
-//   }
-//   next()
+app.use(express.static('static'))
+app.use('/api/myapi',myapiRoute)
+app.use('/api/fruits',fruitRoute)
+app.use(Router)
+/** hello */
+app.use(
+  "/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+// app.get('*',(req,res,next)=>{
+//   res.sendFile(`${process.cwd()}/static/index.html`)
 // })
 
-app.get('/hello',(req,res)=>{
-    
-    res.send("Hello 'YOKA'")
-})
-app.get('/',(req,res)=>{
-    res.send("Hello Express")
-})
-app.listen(port, () => console.log(`running on port ${port}`))
+app.listen(port, () => console.log(`Application is running on port http://localhost/${port}`))
